23fcabac7994d9904dc0b82b0006dc9335c623d0
Gevorg Hovhannisyan <gev9911@gmail.com>
2024-11-24 16:19:14 +0400

    [D-Link:clean-up] Add t_error_log struct for log path and include it in Configuration struct.

--- sources/load-config/load-config.c
+++ sources/load-config/load-config.c
@@ -2,12 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-
-#include "logger-system.h"
 #include <dotenv.h>
 
+#include "logger-system.h"
+#include "panic.h"
+
 static void init() {
-    logger(TRACE, "Load environment variables from the specified file ...");
+    logger(INFO, "Loading environment variables from the specified file ...");
     const char *env_file_path = ".env";
 
     if (env_load(env_file_path, true) != 0) {
@@ -16,22 +17,18 @@ static void init() {
     }
 }
 
-void load_config(t_config* config) {
-    init();
-
-    config->host = getEnvLocal("SERVER_HOST", NULL);
-    if (config->host == NULL) {
-        config->host = "127.0.0.1";
-    }
-
-    char* port_str = getEnvLocal("SERVER_PORT", NULL);
-    if (port_str != NULL) {
-        config->port = atoi(port_str);
-    }
+void init_config(t_config* config) {
+    config->host = NULL;
+    config->port = 0;
+    init_log(&config->log_path);
 }
 
 char* getEnvLocal(const char* env, const char* fallback) {
     const char* environment = getenv(env);
+
+    if(environment == NULL) {
+        logger(ERROR, "MESSSSSSSSSSSSSSSSSSSSSSSSSSSSSSSS");
+    }
 
     if (fallback != NULL) {
         return strdup(fallback);
@@ -42,7 +39,33 @@ char* getEnvLocal(const char* env, const char* fallback) {
     return NULL;
 }
 
+void load_config(t_config* config) {
+    init();
+
+    config->host = getEnvLocal("SERVER_HOST", NULL);
+    if (config->host == NULL) {
+        config->host = "127.0.0.1";
+    }
+
+    char* temp_port = getEnvLocal("SERVER_PORT", NULL);
+    if (temp_port != NULL) {
+        config->port = atoi(temp_port);
+    }
+
+    char* temporary_server = getEnvLocal("SERVER_LOGS_FILE", NULL);
+    if (temporary_server != NULL) {
+        config->log_path.log_server_path = "/var/log/multi-clean-operation/server-logs.log";
+    }
+
+    char* temporary_client = getEnvLocal("CLIENT_LOGS_FILE", NULL);
+    if (temporary_client != NULL) {
+        config->log_path.log_cilent_path = "/var/log/multi-clean-operation/client-logs.log";
+    }
+}
+
 void print_config(t_config config) {
     printf("Host: %s%s", config.host, "\n");
     printf("Host: %d%s", config.port, "\n");
+    printf("ServerLogsPath: %s%s", config.log_path.log_server_path, "\n");
+    printf("ClientLogsPath: %s%s", config.log_path.log_cilent_path, "\n");
 }
