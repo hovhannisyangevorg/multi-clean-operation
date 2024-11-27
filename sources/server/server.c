c60304ccf46d0159bcadd73d78f8ecdc58e09192
Gevorg Hovhannisyan <gev9911@gmail.com>
2024-11-27 14:13:24 +0400

    [D-Link:clean-up] Refactor code and add `-fsanitize` flags for debugging.

--- sources/server/server.c
+++ sources/server/server.c
@@ -1,12 +1,8 @@
-#include <stdlib.h>
-#include <arpa/inet.h>
-#include "logger-system.h"
-#include <arpa/inet.h>
-
 #include "server.h"
-#include "logger-system.h"
 
-t_config ConfigServer;
+typedef struct s_server t_server;
+
+t_config    ConfigServer;
 
 static void init() {
     logger(INFO, "Server configuration loading started...");
@@ -15,28 +11,45 @@ static void init() {
     print_config(ConfigServer);
 }
 
-void server() {
+t_error* server(t_error* Error) {
     logger(INFO, "Server started ...");
     init();
 
-//    int socket_fd;
-//    struct sockaddr_in server_addr;
-//
-//    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
-//    if (socket_fd == -1) {
-//        logger(ERROR, "Socket creation failed");
-//        exit(EXIT_FAILURE);
-//    }
-//
-//    server_addr.sin_family = AF_INET;
-//    server_addr.sin_port = htons(ConfigServer.port);
-//    server_addr.sin_addr.s_addr = inet_addr(ConfigServer.host);
-//
-//    if (server_addr.sin_addr.s_addr == INADDR_NONE) {
-//        logger(ERROR, "Invalid IP address");
-//        exit(EXIT_FAILURE);
-//    }
+    int socket_fd;
+    struct sockaddr_in server_addr;
 
-//    printf("Server logs path: %s\n", SERVER_LOGS);
-//    printf("Client logs path: %s\n", CLIENT_LOGS);
+    socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_fd == -1) {
+        return (Set(Error, format(__func__, "(Socket creation failed.)")));
+    }
+
+    server_addr.sin_family          = AF_INET;
+    server_addr.sin_port            = htons(ConfigServer.port);
+    server_addr.sin_addr.s_addr     = inet_addr(ConfigServer.host);
+
+    if (server_addr.sin_addr.s_addr == INADDR_NONE) {
+        close(socket_fd);
+        return (Set(Error, format(__func__, "(Invalid IP address.)")));
+    }
+
+    if (bind(socket_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        close(socket_fd);
+        return (Set(Error, format(__func__, "(Bind failed)")));
+    }
+
+    if (listen(socket_fd, 3) < 0) {
+        close(socket_fd);
+        return (Set(Error, format(__func__, "(Listen failed)")));
+    }
+
+    printf("Server listening on port %d...\n", ConfigServer.port);
+    return Error;
 }
+
+
+//struct sockaddr_in {
+//    short            sin_family;   // Address family (e.g., AF_INET for IPv4)
+//    unsigned short   sin_port;     // Port number (in network byte order)
+//    struct in_addr   sin_addr;     // IP address (in network byte order)
+//    char             sin_zero[8]; // Padding to match `struct sockaddr` size
+//};
--- sources/server/server.h
+++ sources/server/server.h
@@ -1,9 +1,14 @@
 #ifndef SERVER_HPP
 #define SERVER_HPP
 
+#include <stdlib.h>
+#include <arpa/inet.h>
+#include "logger-system.h"
+#include <arpa/inet.h>
 #include "logger-system.h"
 #include "load-config.h"
+#include "error.h"
 
-void server();
+t_error* server(t_error* error);
 
 #endif //SERVER_HPP
